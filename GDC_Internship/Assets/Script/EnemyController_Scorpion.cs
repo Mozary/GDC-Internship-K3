c66e3488304e90cc74681074684382d00c0706e0
Mozary <endingindra2016@gmail.com>
2019-04-27 18:27:36 +0700

    Update EnemyController_Scorpion.cs

--- GDC_Internship/Assets/Script/EnemyController_Scorpion.cs
+++ GDC_Internship/Assets/Script/EnemyController_Scorpion.cs
@@ -6,7 +6,7 @@ public class EnemyController_Scorpion : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody2D rb2d;
-    private Transform target;
+    private Vector3 target = Vector3.zero;
     private CapsuleCollider2D bodyCollider;
     private Bounds patrolBounds;
     private Vector3 ref_velocity = Vector3.zero;
@@ -15,7 +15,7 @@ public class EnemyController_Scorpion : MonoBehaviour
     public float maxSpeed = 30f;
     public float health = 2f;
     public float hitRange = 0.4f;
-    public float waitTime = 2f;
+    public float waitTime = 0f;
 
     private float SmoothMovement = 0.05f;
     private string state = "patroling";
@@ -29,24 +29,26 @@ public class EnemyController_Scorpion : MonoBehaviour
         animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
         patrolBounds = patrolArea.GetComponent<BoxCollider2D>().bounds;
-        target = transform;
-        Debug.Log(transform.position);
-        Debug.Log(target.position);
         Patrol();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, target.position) <= 0.1f)
+        //Check if patrol position is reached
+        if (Vector2.Distance(transform.localPosition, target) <= 0.4f)
         {
+            //Telling the object to wait
             if(waitTime <= 0)
             {
                 Patrol();
+                waitTime = 3f;
+                maxSpeed = 30f;
             }
             else
             {
                 waitTime -= Time.deltaTime;
+                maxSpeed = 0f;
             }
         }
     }
@@ -55,14 +57,14 @@ public class EnemyController_Scorpion : MonoBehaviour
         if (rb2d != null && target != null)
         {
             Move();
-            if (target.position.x > transform.position.x && !faceRight) Flip();
-            else if (target.position.x < transform.position.x && faceRight) Flip();
+            if (target.x > transform.localPosition.x && !faceRight) Flip();
+            else if (target.x < transform.localPosition.x && faceRight) Flip();
         }
     }
     void Patrol()
     {
         float new_patrolPoint = Random.Range(-patrolBounds.extents.x, patrolBounds.extents.x);
-        target.position = new Vector3(new_patrolPoint, transform.position.y, transform.position.z);
+        target = new Vector2(new_patrolPoint, transform.localPosition.y);
     }
     void Move()
     {
