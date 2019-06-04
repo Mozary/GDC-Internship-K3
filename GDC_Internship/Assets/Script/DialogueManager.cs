26325986339537228ff5eb5b5afba13c8044c390
DedeMastra <49666158+DedeMastra@users.noreply.github.com>
2019-06-04 15:55:52 +0800

    Updated
    Dialogue System (belum selesai)

---
+++ GDC_Internship/Assets/Script/DialogueManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public Text nameUI;
+    public Text dialogueUI;
+
+    private Queue<string> ListDialogues;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ListDialogues = new Queue<string>();
+
+    }
+
+    public void InteractOnButton(Dialogue dialogue)
+    {
+        ListDialogues.Clear();
+        nameUI.text = dialogue.name;
+
+        foreach (string sentence in dialogue.ListSentence)
+        {
+            ListDialogues.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (ListDialogues.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = ListDialogues.Dequeue();
+        dialogueUI.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("testii selesai");
+    }
+}
+
