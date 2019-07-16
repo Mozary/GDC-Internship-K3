eea3adbde527fe15839f70fcfc0f403ff75f3cc2
DedeMastra <49666158+DedeMastra@users.noreply.github.com>
2019-07-16 21:55:42 +0800

    temporary files 2
    percobaan lanjutan

---
+++ GDC_Internship/Assets/Script/CutsceneManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneManager : MonoBehaviour
+{
+    private Animator i_player;
+    public PlayableDirector playableDirector;
+
+    private bool tag1 = true;
+    private bool tag2 = true;
+
+    private void Start()
+    {
+        i_player = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (gameObject.activeSelf && tag1)
+        {
+            OnEnable();
+        }
+        if (playableDirector.state != PlayState.Playing && tag2)
+        {
+            i_player.enabled = false;
+            tag2 = false;
+            abababa();
+        }
+    }
+    private void OnEnable()
+    {
+        i_player.applyRootMotion = true;
+        tag1 = false;
+        Debug.Log("kepanggil");
+    }
+    private void abababa()
+    {
+        /*i_player.applyRootMotion = false;*/
+        Debug.Log("kepanggil ondisablenya");
+        i_player.enabled = true;
+        delayedReset();
+    }
+    void delayedReset()
+    {
+        StartCoroutine(delayedReset(0.5f));
+    }
+    private IEnumerator delayedReset(float s)
+    {
+        Debug.Log("kepanggil coroutinenya");
+        yield return new WaitForSeconds(s);
+        i_player.applyRootMotion = false;
+        /*gameObject.SetActive(false);*/
+    }
+}
