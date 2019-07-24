7e888fb23732ead1df63663d9439ab5e2523b3ac
Mozary <endingindra2016@gmail.com>
2019-07-24 10:23:30 +0700

    HUD

---
+++ GDC_Internship/Assets/Script/Handler/Handler_Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Handler_Timer : MonoBehaviour
+{
+    
+    private void Awake()
+    {
+        
+    }
+}
+public class Timer
+{
+    public const float Max_Time_Amount = 15*60;//15 Minutes
+
+    private float timeRemaining;
+    private const float timeDecrement = 1f;
+
+}
