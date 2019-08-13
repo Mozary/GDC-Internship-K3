62e0c3ee85729a86399c545559572c332ff32ebe
Mozary <endingindra2016@gmail.com>
2019-08-13 23:23:35 +0700

    Reworked Chapter Menu

---
+++ GDC_Internship/Assets/Script/Scene/EndingsLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingsLoader : MonoBehaviour
+{
+    [SerializeField] private string SelectScene;
+
+    public void LoadScene()
+    {
+        if (SelectScene != null)
+        {
+            SceneManager.LoadScene(SelectScene);
+        }
+        
+    }
+}
